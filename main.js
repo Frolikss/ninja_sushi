66c66bff47f1e5989ce4b12a9bdd910527e4d4f0
Frolikss <darkness1198@gmail.com>
2022-11-07 17:55:48 +0200

    Initial

---
+++ main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.banner__scroll').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      infinite: true,
+      dots: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false
+    });
+  });
