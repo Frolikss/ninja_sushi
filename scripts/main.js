d2b7004b4a932b7be8ebb3b99d60f36cd4879aed
Frolikss <darkness1198@gmail.com>
2022-11-24 18:00:52 +0200

    sliders json data

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+'use strict'
+
+const bannerURL = 'http://localhost:3000/head';
+const specialURL = 'http://localhost:3000/special';
+
+const wrapper = document.querySelector('.header__wrapper');
+const bannerSliderItems = document.querySelectorAll('.banner__text');
+const specialSliderItems = document.querySelectorAll('.slider__info');
+
+showOverlay();
+fetchSliderData(bannerSliderItems, bannerURL);
+fetchSliderData(specialSliderItems, specialURL);
 
 $(document).ready(function(){
     $('.banner__scroll').slick({
@@ -22,3 +34,48 @@ $(document).ready(function(){
       arrows: true
     });
   });
+
+  function showOverlay() {
+      const headerBurgerBtn = document.querySelector('.header__menu--burger');
+      const headerCaption = document.querySelector('.header__menu--burger--caption');
+
+      headerBurgerBtn.addEventListener('click', event => {
+
+        const headerOverlay = document.querySelector('.header__menu_overlay');
+        const header = document.querySelector('.header');
+
+        if (headerOverlay.classList.contains('header__menu_overlay--show')) {
+          header.classList.remove('overlay');
+          headerOverlay.classList.remove('header__menu_overlay--show');
+          headerCaption.style.display = 'none';
+          headerCaption.style.opacity = '0';
+          configWrapper()
+        } else {
+          header.classList.add('overlay');
+          headerOverlay.classList.add('header__menu_overlay--show');
+          headerCaption.style.display = 'block';
+          headerCaption.style.opacity = '1';
+          configWrapper();
+        }
+      });
+
+      function configWrapper() {
+        const wrapperNode = document.querySelector('.header__wrapper');
+
+        if (wrapperNode) {
+          wrapperNode.replaceWith(...wrapperNode.childNodes);
+        } else {
+          console.log('here');
+        }
+      }
+  }
+
+  function fetchSliderData(slider, url) {
+    const data = fetch(url).then(response => response.json()).then(data => {
+      for (let i = 0; i < slider.length; i++) {
+        slider[i].firstElementChild.textContent = data[i].header;
+      }
+    }).catch(error => console.log(error));
+  }
+
+
