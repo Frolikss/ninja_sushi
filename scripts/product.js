2f905c7c560d09f6afb6e2e87ee3490219b15b8c
Frolikss <darkness1198@gmail.com>
2022-12-07 16:41:44 +0200

    new product page

---
+++ scripts/product.js
@@ -0,0 +1,3 @@
+import { fillCardWithJSON, configCardCounter } from './modules/cardData.js';
+import { showOverlay, backEvent, configBellBtn, configCartBtn, configOrderBtns } from './modules/header.js';
+import { footerQuicktipToggle } from './modules/footer.js';
