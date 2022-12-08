66736573cf4587d3de505251c90e5b42f6c2dfe0
Frolikss <darkness1198@gmail.com>
2022-12-08 15:03:26 +0200

    product logic cart

--- scripts/product.js
+++ scripts/product.js
@@ -12,7 +12,7 @@ setCards();
 function setCards() {
     fetchCards().then(data => {
         const cardTemplate = document.querySelector('.products__slider--template');
-    
+
         const slider = document.querySelector('.products__slider');
 
         data.forEach((dataItem, index) => {
@@ -26,12 +26,12 @@ function setCards() {
                 isHit: card.querySelector('.hit'),
                 isEco: card.querySelector('.products__item--eco'),
                 isSpicy: card.querySelector('.products__item--spicy'),
-                isDisposable: card.querySelector('.products__item--lactose')
             }
 
             setIconsClasses(dataItem, marks);
             setCardContent(dataItem, card);
             setContainsSlider(contains, card);
+            setSwitch(dataItem, card);
         });
     }).then(res => setSlider());
 }
@@ -53,7 +53,6 @@ function setIconsClasses(data, marks) {
         isHit: 'hit_show',
         isEco: 'products__item--eco_show',
         isSpicy: 'products__item--spicy_show',
-        isDisposable: 'products__item--lactose_show'
     }
 
     const keys = Object.keys(classList);
@@ -76,6 +75,15 @@ function setCardContent(data, card) {
     header.textContent = data.header;
     weight.textContent = data.category === 'drinks' ? `Объем: ${data.weight} л` : `Вес: ${data.weight} г`;
     price.textContent = data.price;
+
+    const info = {
+        image: image.src,
+        name: header.textContent,
+        weight: weight.textContent,
+        price: price.textContent
+    }
+
+    setCartBtn(card, info);
 }
 
 function setContainsSlider(contains, card) {
@@ -89,7 +97,51 @@ function setContainsSlider(contains, card) {
         const name = slide.querySelector('.products__item--consist--name');
         const image = slide.querySelector('.products__item--consist--image');
 
+        slide.dataset.lac = item;
         name.textContent = item.replace('lac_', '');
         image.src = `/assets/png/${item.replace('lac_', '')}.png`;
     })
+}
+
+function setSwitch(data, card) {
+    const switchBlock = card.querySelector('.products__item--switch');
+    const switchActiveClass = 'products__item--switch_active';
+
+    if (data.isDisposable) {
+        switchBlock.classList.add(switchActiveClass);
+        return;
+    }
+
+    switchBlock.addEventListener('click', event => {
+        switchBlock.classList.toggle(switchActiveClass);
+        const slides = card.querySelectorAll('.products__item--consist--item');
+
+        slides.forEach(slide => {
+            if (!slide.dataset.lac.includes('lac_')) return;
+            console.log(slide);
+            slide.classList.toggle('products__item--consist--item_nonlac');
+        });
+    });
+}
+
+function setCartBtn(card, info) {
+    const btn = card.querySelector('.products__item--cart');
+
+    btn.addEventListener('click', event => {
+        let storage = JSON.parse(localStorage.getItem('cards')) ?? [];
+
+        let data = {
+            id: card.dataset.id,
+            image: info.image,
+            name: info.header,
+            weight: info.weight,
+            price: +info.price.replace('грн.', ''),
+            amount: 1
+        }
+
+        let existingCard = storage.find(({ id }) => id === data.id);
+        existingCard ? existingCard.amount++ : storage.push(data);
+    
+        localStorage.setItem('cards', JSON.stringify(storage));
+    });
 }
